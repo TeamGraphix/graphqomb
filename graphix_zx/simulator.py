837d97d4aaa83d49805daefc07ea50c292fb80e9
masa10-f <masa1063fuk@gmail.com>
2024-06-28 18:12:15 +0900

    make circuit simulator

--- graphix_zx/simulator.py
+++ graphix_zx/simulator.py
@@ -1,56 +1,10 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, auto
 
-import numpy as np
-from numpy.typing import NDArray
-
-from graphix_zx.interface import MBQCCircuit
-from graphix_zx.statevec import StateVector
-
-
-class GateKind(Enum):
-    """Enum class for gate kind"""
-
-    J = "J"
-    CZ = "CZ"
-    PhaseGadget = "PHASE_GADGET"
-
-
-@dataclass(frozen=True)
-class Gate:
-    kind: GateKind
-
-    def get_matrix(self) -> NDArray:
-        raise NotImplementedError
-
-
-class J(Gate):
-    kind: GateKind = GateKind.J
-    qubit: int
-    angle: float
-
-    def get_matrix(self) -> NDArray:
-        return np.array([[1, np.exp(1j * self.angle)], [1, -np.exp(1j * self.angle)]]) / np.sqrt(2)
-
-
-class CZ(Gate):
-    kind: GateKind = GateKind.CZ
-    qubits: tuple[int, int]
-
-    def get_matrix(self) -> NDArray:
-        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]])
-
-
-class PhaseGadget(Gate):
-    kind: GateKind = GateKind.PhaseGadget
-    qubits: list[int]
-    angle: float
-
-    def get_matrix(self) -> NDArray:
-        raise NotImplementedError
+from graphix_zx.interface import MBQCCircuit, Gate, GateKind
+from graphix_zx.statevec import BaseStateVector, StateVector
 
 
 class BaseSimulator(ABC):
@@ -63,23 +17,19 @@ class BaseSimulator(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get_state(self) -> NDArray:
+    def simulate(self):
         raise NotImplementedError
 
     @abstractmethod
-    def simulate(self) -> NDArray:
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_measurement(self) -> NDArray:
+    def get_state(self) -> BaseStateVector:
         raise NotImplementedError
 
 
 class CircuitSimulatorBackend(Enum):
     """Enum class for circuit simulator backend"""
 
-    StateVector = "StateVector"
-    DensityMatrix = "DensityMatrix"
+    StateVector = auto()
+    DensityMatrix = auto()
 
 
 # NOTE: Currently, only XY plane is supported
@@ -87,14 +37,28 @@ class MBQCCircuitSimulator(BaseSimulator):
     def __init__(self, backend: CircuitSimulatorBackend, mbqc_circuit: MBQCCircuit):
         # NOTE: is it a correct backend switch?
         if backend == CircuitSimulatorBackend.StateVector:
-            self.__state_vector = StateVector(mbqc_circuit.num_qubits)
+            self.__state = StateVector(mbqc_circuit.num_qubits)
         elif backend == CircuitSimulatorBackend.DensityMatrix:
             raise NotImplementedError
         else:
             raise ValueError("Invalid backend")
 
-        self.__num_qubits = mbqc_circuit.num_qubits
-        self.__front_nodes: set[int] = set()
+        self.__gate_instructions: list[Gate] = mbqc_circuit.get_instructions()
 
     def apply_gate(self, gate: Gate):
-        raise NotImplementedError
+        operator = gate.get_matrix()
+        if gate.kind == GateKind.J:
+            self.__state.evolve(operator, [gate.qubit])
+        elif gate.kind == GateKind.CZ:
+            self.__state.evolve(operator, list(gate.qubits))
+        elif gate.kind == GateKind.PhaseGadget:
+            raise NotImplementedError
+        else:
+            raise ValueError("Invalid gate")
+
+    def simulate(self):
+        for gate in self.__gate_instructions:
+            self.apply_gate(gate)
+
+    def get_state(self) -> StateVector:
+        return self.__state
