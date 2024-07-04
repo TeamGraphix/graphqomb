dc8ec1fa49e5da82a2e82c058642fab61e7bc487
masa10-f <masa1063fuk@gmail.com>
2024-07-04 22:11:11 +0900

    seed for resource state optimization

---
+++ graphix_zx/graph_opt.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from graphix_zx.interface import GraphState
+from graphix_zx.focus_flow import GFlow
+
+
+def minimize_space(graph: GraphState, gflow: GFlow) -> list[GraphState]:
+    """Minimize the space of the graph."""
+    raise NotImplementedError
