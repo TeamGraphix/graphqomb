fix sign mismatch in J

graphix_zx/circuit.py
@@ -33,7 +33,7 @@ class J(Gate):
     angle: float = 0
 
     def get_matrix(self) -> NDArray:
-        return np.array([[1, np.exp(1j * self.angle)], [1, -np.exp(1j * self.angle)]]) / np.sqrt(2)
+        return np.array([[1, np.exp(-1j * self.angle)], [1, -np.exp(-1j * self.angle)]]) / np.sqrt(2)
 
 
 @dataclass(frozen=True)

tests/test_circuit.py
@@ -41,10 +41,10 @@ def test_mbqc_circuit_simulator():
     expected_state = (
         np.array(
             [
-                (1 + np.exp(1j * np.pi / 4)),
-                (1 + np.exp(1j * np.pi / 4)),
-                (1 - np.exp(1j * np.pi / 4)),
-                -(1 - np.exp(1j * np.pi / 4)),
+                (1 + np.exp(-1j * np.pi / 4)),
+                (1 + np.exp(-1j * np.pi / 4)),
+                (1 - np.exp(-1j * np.pi / 4)),
+                -(1 - np.exp(-1j * np.pi / 4)),
             ],
             dtype=complex,
         )