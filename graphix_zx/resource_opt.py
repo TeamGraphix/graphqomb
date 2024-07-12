0c13247f06a2a4a69eba1322fe7cfd1b2e8517bc
masa10-f <masa1063fuk@gmail.com>
2024-07-12 20:01:16 +0900

    rename graph_opt -> resource_opt

--- graphix_zx/resource_opt.py
+++ graphix_zx/resource_opt.py
@@ -4,7 +4,9 @@ from graphix_zx.interface import GraphState, BasicGraphState
 from graphix_zx.focus_flow import GFlow, construct_dag
 
 
-def get_subgraph_sequences(graph: GraphState, meas_order: list[int]) -> list[GraphState]:
+def get_subgraph_sequences(
+    graph: GraphState, meas_order: list[int]
+) -> list[GraphState]:
     """Get the subgraph sequences."""
     subgraphs = []
     activated_nodes = graph.input_nodes
@@ -38,7 +40,9 @@ def get_minimized_sp_meas_order(graph: GraphState, gflow: GFlow) -> list[int]:
     """Get the minimized space measurement order."""
     inverted_dag = get_dependencies(graph, gflow)
     activated_nodes = set(graph.input_nodes)
-    unmeasured_nodes = graph.get_physical_nodes() - activated_nodes - set(graph.output_nodes)
+    unmeasured_nodes = (
+        graph.get_physical_nodes() - activated_nodes - set(graph.output_nodes)
+    )
 
     meas_order = []
 
@@ -52,11 +56,16 @@ def get_minimized_sp_meas_order(graph: GraphState, gflow: GFlow) -> list[int]:
             raise ValueError("Cycle detected in the graph")
 
         # evaluate activation cost
-        activation_costs = {node: count_activation_cost(graph, node, activated_nodes) for node in meas_candidates}
+        activation_costs = {
+            node: count_activation_cost(graph, node, activated_nodes)
+            for node in meas_candidates
+        }
         # select the node with the minimum activation cost
         target_node = min(activation_costs, key=activation_costs.get)
         meas_order.append(target_node)
-        activated_nodes |= get_activation_nodes(graph, target_node, activated_nodes) | {target_node}
+        activated_nodes |= get_activation_nodes(graph, target_node, activated_nodes) | {
+            target_node
+        }
         unmeasured_nodes -= {target_node}
     return meas_order
 
@@ -71,14 +80,18 @@ def get_dependencies(graph: GraphState, gflow: GFlow) -> dict[int, set[int]]:
     return inverted_dag
 
 
-def get_activation_nodes(graph: GraphState, target_node: int, activated_nodes: set[int]) -> set[int]:
+def get_activation_nodes(
+    graph: GraphState, target_node: int, activated_nodes: set[int]
+) -> set[int]:
     """Get the nodes to be activated."""
     neighbors = set(graph.get_neighbors(target_node))
     activation_nodes = neighbors - activated_nodes
     return activation_nodes
 
 
-def count_activation_cost(graph: GraphState, target_node: int, activated_nodes: set[int]) -> int:
+def count_activation_cost(
+    graph: GraphState, target_node: int, activated_nodes: set[int]
+) -> int:
     """Count the activation cost."""
     activation_nodes = get_activation_nodes(graph, target_node, activated_nodes)
     return len(activation_nodes)
