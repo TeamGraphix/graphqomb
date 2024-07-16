4296e2f2032beb672c022d593a528bffceecdf3f
masa10-f <masa1063fuk@gmail.com>
2024-07-16 21:58:23 +0900

    fix test_pattern

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -11,55 +11,55 @@ from graphix_zx.pattern import NodeAlreadyPreparedError, MutablePattern, is_stan
 
 
 def test_pattern_initialization():
-    pattern = MutablePattern([0, 1, 2])
-    assert pattern.get_input_nodes() == [0, 1, 2]
-    assert pattern.get_output_nodes() == [0, 1, 2]
+    pattern = MutablePattern({0, 1, 2})
+    assert pattern.get_input_nodes() == {0, 1, 2}
+    assert pattern.get_output_nodes() == {0, 1, 2}
     assert len(pattern) == 0
 
 
 def test_pattern_add_command():
-    pattern = MutablePattern([0, 1])
+    pattern = MutablePattern({0, 1})
     cmd = N(node=2)
     pattern.add(cmd)
     assert len(pattern) == 1
-    assert pattern.get_output_nodes() == [0, 1, 2]
+    assert pattern.get_output_nodes() == {0, 1, 2}
 
 
 def test_pattern_add_command_already_prepared():
-    pattern = MutablePattern([0, 1])
+    pattern = MutablePattern({0, 1})
     cmd = N(node=1)
     with pytest.raises(NodeAlreadyPreparedError):
         pattern.add(cmd)
 
 
 def test_pattern_add_measurement_command():
-    pattern = MutablePattern([0, 1])
+    pattern = MutablePattern({0, 1})
     cmd = M(node=1)
     pattern.add(cmd)
     assert len(pattern) == 1
-    assert pattern.get_output_nodes() == [0]  # 1 is measured and removed from output_nodes
+    assert pattern.get_output_nodes() == {0}  # 1 is measured and removed from output_nodes
 
 
 def test_pattern_clear():
-    pattern = MutablePattern([0, 1])
+    pattern = MutablePattern({0, 1})
     cmd = N(node=2)
     pattern.add(cmd)
     pattern.clear()
     assert len(pattern) == 0
-    assert pattern.get_output_nodes() == [0, 1]
+    assert pattern.get_output_nodes() == {0, 1}
 
 
 def test_pattern_replace():
-    pattern = MutablePattern([0, 1])
+    pattern = MutablePattern({0, 1})
     cmds = [N(node=2), M(node=3)]
-    pattern.replace(cmds, input_nodes=[3, 4])
-    assert pattern.get_input_nodes() == [3, 4]
-    assert pattern.get_output_nodes() == [4, 2]
+    pattern.replace(cmds, input_nodes={3, 4})
+    assert pattern.get_input_nodes() == {3, 4}
+    assert pattern.get_output_nodes() == {4, 2}
     assert len(pattern) == 2  # 2 replaced commands
 
 
 def test_pattern_get_space_list():
-    pattern = MutablePattern([0])
+    pattern = MutablePattern({0})
     cmds = [
         N(node=1),
         N(node=2),
@@ -71,7 +71,7 @@ def test_pattern_get_space_list():
 
 
 def test_pattern_calc_max_space():
-    pattern = MutablePattern([0])
+    pattern = MutablePattern({0})
     cmds = [
         N(node=1),
         N(node=2),
@@ -83,7 +83,7 @@ def test_pattern_calc_max_space():
 
 
 def test_is_standardized():
-    pattern = MutablePattern([0])
+    pattern = MutablePattern({0})
     cmds = [
         N(node=1),
         E(nodes=(0, 1)),
