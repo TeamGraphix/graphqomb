6a62e30bef884fd6c0caa4171dd323e22ed9e21f
masa10-f <masa1063fuk@gmail.com>
2024-10-14 18:43:53 +0900

    add docstring on command

--- graphix_zx/command.py
+++ graphix_zx/command.py
@@ -1,4 +1,15 @@
-"""Data validator command classes."""
+"""Command module for measurement pattern.
+
+This module provides:
+- N: Preparation command.
+- M: Measurement command.
+- E: Entanglement command.
+- C: Clifford command.
+- Correction: Base class for Pauli correction command.
+- X: X correction command.
+- Z: Z correction command.
+- Command: Type alias of all commands.
+"""
 
 from __future__ import annotations
 
@@ -16,7 +27,16 @@ Node = int
 
 @dataclass
 class N:
-    """Preparation command."""
+    """Preparation command.
+
+    Attributes
+    ----------
+    node : int
+        The node index to be prepared.
+    q_index : int
+        The logical qubit index.
+        If not provided, it is set to -1(ancilla).
+    """
 
     node: Node
     q_index: int = -1
@@ -24,7 +44,28 @@ class N:
 
 @dataclass
 class M:
-    """Measurement command. By default the plane is set to 'XY', the angle to 0, empty domains and identity vop."""
+    """Measurement command.
+
+    Attributes
+    ----------
+    node : int
+        The node index to be measured.
+    plane : Plane
+        The measurement plane.
+    angle : float
+        The measurement angle.
+    s_domain : set[int]
+        The s_domain of the measurement.
+    t_domain : set[int]
+        The t_domain of the measurement.
+
+    See Also
+    --------
+    graphix_zx.simulator.PatternSimulator._apply_m :
+        The s_domain and t_domain follow the same definition in the original paper
+        Journal of the ACM (JACM), Volume 54, Issue 2.
+
+    """
 
     node: Node
     plane: Plane = Plane.XY
@@ -35,14 +76,28 @@ class M:
 
 @dataclass
 class E:
-    """Entanglement command."""
+    """Entanglement command.
+
+    Attributes
+    ----------
+    nodes : tuple[int, int]
+        The node indices to be entangled.
+    """
 
     nodes: tuple[Node, Node]
 
 
 @dataclass
 class C:
-    """Clifford command."""
+    """Clifford command.
+
+    Attributes
+    ----------
+    node : int
+        The node index to apply the local Clifford.
+    local_clifford : LocalClifford
+        The local Clifford to apply
+    """
 
     node: Node
     local_clifford: LocalClifford
@@ -50,7 +105,15 @@ class C:
 
 @dataclass
 class Correction:
-    """Correction command.Either X or Z."""
+    """Correction command.Either X or Z.
+
+    Attributes
+    ----------
+    node : int
+        The node index to apply the correction.
+    domain : set[int]
+        The domain of the correction
+    """
 
     node: Node
     domain: set[Node] = field(default_factory=set)
