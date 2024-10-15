d9a6db0434f4f067cb4e70e29be12f9d28e6addd
masa10-f <masa1063fuk@gmail.com>
2024-10-15 20:10:21 +0900

    add docstring on simulator_backend

--- graphix_zx/simulator_backend.py
+++ graphix_zx/simulator_backend.py
@@ -1,3 +1,5 @@
+"""The base class for simulator backends."""
+
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
@@ -13,27 +15,73 @@ if TYPE_CHECKING:
 
 # backend for all simulator backends
 class BaseSimulatorBackend(ABC):
+    """Base class for simulator backends."""
+
     @property
     @abstractmethod
     def num_qubits(self) -> int:
+        """Get the number of qubits in the state.
+
+        Returns
+        -------
+        int
+            The number of qubits in the state.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def evolve(self, operator: NDArray, qubits: Sequence[int]) -> None:
+        """Evolve the state by applying an operator to a subset of qubits.
+
+        Parameters
+        ----------
+        operator : NDArray
+            The operator to apply.
+        qubits : Sequence[int]
+            The qubits to apply the operator to.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def measure(self, qubit: int, plane: Plane, angle: float, result: int) -> None:
+        """Measure a qubit in a given measurement basis.
+
+        Parameters
+        ----------
+        qubit : int
+            The qubit to measure.
+        plane : Plane
+            The measurement plane.
+        angle : float
+            The measurement angle.
+        result : int
+            The measurement result.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def normalize(self) -> None:
+        """Normalize the state."""
         raise NotImplementedError
 
     @abstractmethod
     def get_norm(self) -> float:
+        """Get the norm of the state.
+
+        Returns
+        -------
+        float
+            The norm of the state.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_array(self) -> NDArray:
+        """Get the state as a numpy array.
+
+        Returns
+        -------
+        NDArray
+            The state as a numpy array.
+        """
         raise NotImplementedError
