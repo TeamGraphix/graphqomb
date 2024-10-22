dddc104cd21b391b757e579c8c16658be9ddbf42
Masato Fukushima <masa1063fuk@gmail.com>
2024-10-22 18:45:04 +0900

    WIP add test for euler

---
+++ tests/test_euler.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pytest
+
+from graphix_zx.common import MeasBasis, Plane, get_meas_basis
+from graphix_zx.euler import (
+    LocalClifford,
+    LocalUnitary,
+    _get_meas_basis_info,
+    _is_close_angle,
+    euler_decomposition,
+    is_clifford_angle,
+    update_lc_basis,
+    update_lc_lc,
+)
+from graphix_zx.matrix import is_unitary
+
+
+@pytest.fixture
+def rng() -> np.random.Generator:
+    return np.random.default_rng()
+
+
+@pytest.fixture
+def random_angles(rng: np.random.Generator) -> tuple[float, float, float]:
+    return tuple(rng.uniform(0, 2 * np.pi, 3))
+
+
+@pytest.fixture
+def random_clifford_angles(rng: np.random.Generator) -> tuple[float, float, float]:
+    return tuple(rng.choice([0, np.pi / 2, np.pi, 3 * np.pi / 2], 3))
+
+
+def test_identity() -> None:
+    lc = LocalUnitary(0, 0, 0)
+    assert np.allclose(lc.get_matrix(), np.eye(2))
+
+
+def test_unitary(random_angles: tuple[float, float, float]) -> None:
+    lc = LocalUnitary(*random_angles)
+    assert is_unitary(lc.get_matrix())
+
+
+@pytest.mark.skip
+def test_euler_decomposition(random_angles: tuple[float, float, float]) -> None:
+    array = LocalUnitary(*random_angles).get_matrix()
+    alpha, beta, gamma = euler_decomposition(array)
+
+    assert _is_close_angle(alpha, random_angles[0])
+    assert _is_close_angle(beta, random_angles[1])
+    assert _is_close_angle(gamma, random_angles[2])
+
+
+@pytest.mark.skip
+def test_get_bloch_sphere_coordinates(rng: np.random.Generator) -> None:
+    for plane in Plane:
+        angle = rng.uniform(0, 2 * np.pi)
+        basis = get_meas_basis(plane, angle)
+        plane_get, angle_get = _get_meas_basis_info(basis)
+        assert plane == plane_get, f"Expected {plane}, got {plane_get}"
+        assert _is_close_angle(angle, angle_get), f"Expected {angle}, got {angle_get}"
+
+
+def test_local_clifford(random_clifford_angles: tuple[float, float, float]) -> None:
+    lc = LocalClifford(*random_clifford_angles)
+    assert is_unitary(lc.get_matrix())
+
+    assert is_clifford_angle(lc.alpha)
+    assert is_clifford_angle(lc.beta)
+    assert is_clifford_angle(lc.gamma)
+
+
+@pytest.mark.skip
+def test_lc_lc_update(random_angles: tuple[float, float, float]) -> None:
+    lc1 = LocalClifford(*random_angles)
+    lc2 = LocalClifford(*random_angles)
+    lc = update_lc_lc(lc1, lc2)
+    assert is_unitary(lc.get_matrix())
+
+    assert is_clifford_angle(lc.alpha)
+    assert is_clifford_angle(lc.beta)
+    assert is_clifford_angle(lc.gamma)
+
+
+@pytest.mark.skip
+@pytest.mark.parametrize("plane", [Plane.XY, Plane.YZ, Plane.ZX])
+def test_lc_basis_update(plane: Plane, random_angles: tuple[float, float, float], rng: np.random.Generator) -> None:
+    lc = LocalClifford(*random_angles)
+    angle = rng.uniform(0, 2 * np.pi)
+    basis = MeasBasis(plane, angle)
+    basis_updated = update_lc_basis(lc, basis)
+    assert np.allclose(basis_updated.get_vector(), lc.get_matrix() @ basis.get_vector())
+    assert basis_updated.plane == plane
+    assert _is_close_angle(basis_updated.angle, angle)
