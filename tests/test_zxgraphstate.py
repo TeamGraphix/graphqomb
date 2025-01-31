69632065cad892974180c3198e8fe5d9b85b44e3
Masato Fukushima <masa1063fuk@gmail.com>
2025-01-31 20:31:14 +0900

    fix ruff errors

--- tests/test_zxgraphstate.py
+++ tests/test_zxgraphstate.py
@@ -98,7 +98,7 @@ def test_local_complement_fails_with_input_node(zx_graph: ZXGraphState) -> None:
     """Test local complement fails with input node."""
     zx_graph.add_physical_node(1)
     zx_graph.set_input(1)
-    with pytest.raises(ValueError, match="Cannot apply local complement to input node."):
+    with pytest.raises(ValueError, match=r"Cannot apply local complement to input node."):
         zx_graph.local_complement(1)
 
 
@@ -863,7 +863,7 @@ def test_unremovable_clifford_vertex(zx_graph: ZXGraphState) -> None:
         (3, Plane.XY, 0.5 * np.pi),
     ]
     _apply_measurements(zx_graph, measurements)
-    with pytest.raises(ValueError, match="This Clifford vertex is unremovable."):
+    with pytest.raises(ValueError, match=r"This Clifford vertex is unremovable."):
         zx_graph.remove_clifford(2)
 
 
