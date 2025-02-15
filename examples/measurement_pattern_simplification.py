9a7c3a75c1c9048308d8d6fbffc7d189c63c6d4b
nabe98 <watanabeyuki@watanabeyuumarenoMacBook-Pro.local>
2025-02-15 23:09:32 +0900

    :sparkles: Add an example for Clifford/non-Clifford removal

---
+++ examples/measurement_pattern_simplification.py
@@ -0,0 +1,42 @@
+"""Basic example of simplifying a measurement pattern via a ZX-diagram simplification.
+
+By using the `prune_non_cliffords` method,
+we can remove certain Clifford nodes and non-Clifford nodes from the ZX-diagram,
+which can simplify the resulting measurement pattern.
+"""
+
+# %%
+from copy import deepcopy
+
+import numpy as np
+
+from graphix_zx.circuit import circuit2graph
+from graphix_zx.random_objects import get_random_gflow_circ
+from graphix_zx.visualizer import visualize
+from graphix_zx.zxgraphstate import ZXGraphState
+
+# %%
+circ = get_random_gflow_circ(4, 4, angle_list=[0, np.pi / 3, 2 * np.pi / 3, np.pi])
+graph, flow = circuit2graph(circ)
+zx_graph = ZXGraphState()
+zx_graph.append(graph)
+
+visualize(zx_graph)
+print("node | plane | angle (/pi)")
+for node in zx_graph.input_nodes:
+    print(f"{node} (input)", zx_graph.meas_bases[node].plane, zx_graph.meas_bases[node].angle / np.pi)
+for node in zx_graph.physical_nodes - zx_graph.input_nodes - zx_graph.output_nodes:
+    print(node, zx_graph.meas_bases[node].plane, zx_graph.meas_bases[node].angle / np.pi)
+
+# %%
+zx_graph_smp = deepcopy(zx_graph)
+zx_graph_smp.prune_non_cliffords()
+
+visualize(zx_graph_smp)
+print("node | plane | angle (/pi)")
+for node in zx_graph.input_nodes:
+    print(f"{node} (input)", zx_graph.meas_bases[node].plane, zx_graph.meas_bases[node].angle / np.pi)
+for node in zx_graph_smp.physical_nodes - zx_graph.input_nodes - zx_graph_smp.output_nodes:
+    print(node, zx_graph_smp.meas_bases[node].plane, zx_graph_smp.meas_bases[node].angle / np.pi)
+
+# %%
