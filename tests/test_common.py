4a617d40c7ed52508d7a61da794da48bba140013
Masato Fukushima <masa1063fuk@gmail.com>
2025-02-17 19:08:15 +0900

    add unit test

---
+++ tests/test_common.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pytest
+
+from graphix_zx.common import Plane, PlannerMeasBasis
+
+
+def test_inverse_order_plane() -> None:
+    with pytest.raises(AttributeError):
+        _ = PlannerMeasBasis(Plane.YX, 0)  # type: ignore[attr-defined]
+    with pytest.raises(AttributeError):
+        _ = PlannerMeasBasis(Plane.ZX, 0)  # type: ignore[attr-defined]
+    with pytest.raises(AttributeError):
+        _ = PlannerMeasBasis(Plane.ZY, 0)  # type: ignore[attr-defined]
