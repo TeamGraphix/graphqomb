acc536eab1bd906e542f9bafa943f107adf53f7b
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-04-15 17:53:15 +0900

    :recycle: Remove unnecessary TypeVar

--- graphix_zx/matrix.py
+++ graphix_zx/matrix.py
@@ -7,22 +7,20 @@ This module provides:
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING
 
 import numpy as np
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
 
-T = TypeVar("T", bound=np.number[Any])
 
-
-def is_unitary(mat: NDArray[T]) -> bool:
+def is_unitary(mat: NDArray[np.number]) -> bool:
     r"""Check if a matrix is unitary.
 
     Parameters
     ----------
-    mat : `numpy.typing.NDArray`\[T]
+    mat : `numpy.typing.NDArray`\[`numpy.number`\]
         matrix to check
 
     Returns
