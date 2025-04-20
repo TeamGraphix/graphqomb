58338d217dfc06b3c3922f21fbe2a599c500db9b
masa10-f <masa1063fuk@gmail.com>
2025-04-20 19:40:34 +0900

    revert to use TypeVar

--- graphix_zx/matrix.py
+++ graphix_zx/matrix.py
@@ -7,20 +7,22 @@ This module provides:
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, TypeVar
 
 import numpy as np
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
 
+T = TypeVar("T", bound=np.number[Any])
 
-def is_unitary(mat: NDArray[np.number]) -> bool:
+
+def is_unitary(mat: NDArray[T]) -> bool:
     r"""Check if a matrix is unitary.
 
     Parameters
     ----------
-    mat : `numpy.typing.NDArray`\[`numpy.number`\]
+    mat : `numpy.typing.NDArray`\[T\]
         matrix to check
 
     Returns
