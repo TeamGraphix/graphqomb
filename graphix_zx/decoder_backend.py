5b21a096a5ff9645f72f72443c1a4634c5f6115c
Masato Fukushima <masa1063fuk@gmail.com>
2025-05-03 17:19:30 +0900

    fix API for decoder backend

--- graphix_zx/decoder_backend.py
+++ graphix_zx/decoder_backend.py
@@ -3,27 +3,46 @@
 import operator
 from abc import ABC, abstractmethod
 from functools import reduce
-from typing import Any
 
 
 class BaseDecoder(ABC):
+    """Base class for decoders."""
+
     @abstractmethod
-    def decode(self, cbits: dict[int, bool]) -> Any:
-        pass
-
-
-class XORDecoder(BaseDecoder):
-    def decode(self, input_cbits: dict[int, bool]) -> bool:
+    def decode(self, intput_cbits: dict[int, bool], output_cbits: list[int]) -> dict[int, bool]:
         """Decode the input classical bits to a single bit.
 
         Parameters
         ----------
         input_cbits : dict[int, bool]
             A dictionary mapping bit positions to their boolean values.
+        output_cbits : list[int]
+            A list of bit positions to be decoded.
 
         Returns
         -------
-        bool
-            The result of XOR operation on the input bits.
+        dict[int, bool]
+            Decoded results as a dictionary mapping bit positions to their boolean values.
         """
-        return reduce(operator.xor, input_cbits.values(), False)
+
+
+class XORDecoder(BaseDecoder):
+    """XOR decoder class."""
+
+    def decode(self, input_cbits: dict[int, bool], output_cbits: list[int]) -> dict[int, bool]:  # noqa: PLR6301
+        """Decode the input classical bits to a single bit.
+
+        Parameters
+        ----------
+        input_cbits : dict[int, bool]
+            A dictionary mapping bit positions to their boolean values.
+        output_cbits : list[int]
+            A list of bit positions to be decoded.
+
+        Returns
+        -------
+        dict[int, bool]
+            The results of XOR operation on the input bits.
+        """
+        result = reduce(operator.xor, input_cbits.values(), False)
+        return dict.fromkeys(output_cbits, result)
