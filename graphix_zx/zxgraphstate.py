a0baef722e8ebc82055b9b40b2fdfa47920bc523
nabe98 <watanabeyuki@watanabeyuumarenoMacBook-Pro.local>
2025-05-04 01:28:12 +0900

    :fire: Remove _update_new_measurement method

--- graphix_zx/zxgraphstate.py
+++ graphix_zx/zxgraphstate.py
@@ -58,23 +58,6 @@ class ZXGraphState(GraphState):
         for edge in new_edges:
             self.add_physical_edge(edge[0], edge[1])
 
-    def _update_node_measurement(
-        self, measurement_action: Mapping[Plane, tuple[Plane, Callable[[float], float]]], v: int
-    ) -> None:
-        """Update the measurement action of the node.
-
-        Parameters
-        ----------
-        measurement_action : Mapping[Plane, tuple[Plane, Callable[[float], float]]]
-            mapping of the measurement plane to the new measurement plane and function to update the angle
-        v : int
-            node index
-        """
-        new_plane, new_angle_func = measurement_action[self.meas_bases[v].plane]
-        if new_plane:
-            new_angle = new_angle_func(v) % (2.0 * np.pi)
-            self.set_meas_basis(v, PlannerMeasBasis(new_plane, new_angle))
-
     def local_complement(self, node: int) -> None:
         """Local complement operation on the graph state: G*u.
 
