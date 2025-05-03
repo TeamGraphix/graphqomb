7cb3dd01bcb6fae6655d6300788df690b1da5001
Masato Fukushima <masa1063fuk@gmail.com>
2025-05-03 13:09:46 +0900

    draft implementation of command

---
+++ graphix_zx/command.py
@@ -0,0 +1,120 @@
+"""Command module for measurement pattern.
+
+This module provides:
+
+- `N`: Preparation command.
+- `M`: Measurement command.
+- `E`: Entanglement command.
+- `C`: Clifford command.
+- `Correction`: Base class for Pauli correction command.
+- `X`: X correction command.
+- `Z`: Z correction command.
+- `Command`: Type alias of all commands.
+"""
+
+from __future__ import annotations
+
+import sys
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Union
+
+from graphix_zx.common import MeasBasis, default_meas_basis
+
+if TYPE_CHECKING:
+    from graphix_zx.euler import LocalClifford
+
+
+@dataclass
+class N:
+    """Preparation command.
+
+    Attributes
+    ----------
+    node : `int`
+        The node index to be prepared.
+    """
+
+    node: int
+
+
+@dataclass
+class M:
+    r"""Measurement command.
+
+    Attributes
+    ----------
+    node : `int`
+        The node index to be measured.
+    meas_basis : MeasBasis
+        The measurement basis.
+    s_domain : `set`\[`int`\]
+        The s_domain of the measurement.
+    t_domain : `set`\[`int`\]
+        The t_domain of the measurement.
+    """
+
+    node: int
+    meas_basis: MeasBasis = field(default_factory=default_meas_basis)
+    s_domain: set[int] = field(default_factory=set)
+    t_domain: set[int] = field(default_factory=set)
+
+
+@dataclass
+class E:
+    r"""Entanglement command.
+
+    Attributes
+    ----------
+    nodes : `tuple`\[`int`, `int`\]
+        The node indices to be entangled.
+    """
+
+    nodes: tuple[int, int]
+
+
+@dataclass
+class C:
+    """Clifford command.
+
+    Attributes
+    ----------
+    node : `int`
+        The node index to apply the local Clifford.
+    local_clifford : `LocalClifford`
+        The local Clifford to apply
+    """
+
+    node: int
+    local_clifford: LocalClifford
+
+
+@dataclass
+class Correction:
+    r"""Correction command.Either X or Z.
+
+    Attributes
+    ----------
+    node : `int`
+        The node index to apply the correction.
+    domain : `set`\[`int`\]
+        The domain of the correction
+    """
+
+    node: int
+    domain: set[int] = field(default_factory=set)
+
+
+@dataclass
+class X(Correction):
+    """X correction command."""
+
+
+@dataclass
+class Z(Correction):
+    """Z correction command."""
+
+
+if sys.version_info >= (3, 10):
+    Command = N | M | E | C | X | Z
+else:
+    Command = Union[N, M, E, C, X, Z]
