b2d819fed53bba3b45e22e906a9bbcc1290a2844
masa10-f <masa1063fuk@gmail.com>
2025-06-25 12:32:45 +0900

    update example

--- examples/pattern_generation.py
+++ examples/pattern_generation.py
@@ -8,13 +8,13 @@ using the graphix-zx library.
 
 # %%
 from graphix_zx.pattern import print_pattern
-from graphix_zx.qompiler import qompile_from_flow
+from graphix_zx.qompiler import qompile
 from graphix_zx.random_objects import generate_random_flow_graph
 
 # %%
 # convert circuit to graph and flow
 graphstate, gflow = generate_random_flow_graph(width=3, depth=5)
 # first, qompile it to standardized pattern
-pattern = qompile_from_flow(graphstate, gflow)
+pattern = qompile(graphstate, gflow)
 print("get max space of pattern:", pattern.max_space)
 print_pattern(pattern)
