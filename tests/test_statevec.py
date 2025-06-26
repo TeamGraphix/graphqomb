55438dc0cb356631985348154e4e07cd492efa86
masa10-f <masa1063fuk@gmail.com>
2025-06-26 11:17:37 +0900

    add unittest for statevec

---
+++ tests/test_statevec.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pytest
+
+from graphix_zx.common import Plane, PlannerMeasBasis
+from graphix_zx.statevec import StateVector
+
+
+@pytest.fixture
+def plus_state() -> StateVector:
+    return StateVector(2)
+
+
+@pytest.fixture
+def state_vector() -> StateVector:
+    num_qubits = 3
+    state = np.arange(2**num_qubits, dtype=np.complex128)
+    return StateVector(num_qubits, state)
+
+
+def test_initialization(state_vector: StateVector) -> None:
+    expected_state = np.arange(2**state_vector.num_qubits, dtype=np.complex128)
+    assert state_vector.num_qubits == 3
+    assert np.allclose(state_vector.state, expected_state)
+
+
+def test_evolve(state_vector: StateVector) -> None:
+    operator = np.asarray([[1, 0], [0, -1]])  # Z gate
+    state_vector.evolve(operator, [0])
+    expected_state = np.arange(2**state_vector.num_qubits, dtype=np.complex128)
+    expected_state[len(expected_state) // 2 :] *= -1  # apply Z gate to qubit 0
+    assert np.allclose(state_vector.state, expected_state)
+
+
+def test_measure(state_vector: StateVector) -> None:
+    expected_state = np.arange(2 ** (state_vector.num_qubits - 1), dtype=np.complex128)
+
+    state_vector.measure(0, PlannerMeasBasis(Plane.XZ, 0), 0)  # project onto |0> state
+
+    assert state_vector.num_qubits == 2
+    assert np.allclose(state_vector.state, expected_state)
+
+
+def test_tensor_product(state_vector: StateVector) -> None:
+    expected_state = np.asarray([i // 2 for i in range(2 ** (state_vector.num_qubits + 1))] / np.sqrt(2))
+    other_vector = StateVector(1)
+    state_vector.tensor_product(other_vector)
+
+    assert state_vector.num_qubits == 4
+    assert np.allclose(state_vector.state, expected_state)
+
+
+def test_normalize(state_vector: StateVector) -> None:
+    state_vector.normalize()
+    expected_norm = 1.0
+    assert np.isclose(state_vector.norm(), expected_norm)
+
+
+# def test_reorder(state_vector: StateVector):
+#     pass
+
+
+def test_is_isolated(plus_state: StateVector) -> None:
+    isolated = plus_state.is_isolated(0)
+    assert isolated
+
+
+def test_is_not_isolated(plus_state: StateVector) -> None:
+    cz = np.asarray([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]])
+
+    plus_state.evolve(cz, [0, 1])
+    isolated = plus_state.is_isolated(0)
+    assert not isolated
+
+
+def test_get_norm(state_vector: StateVector) -> None:
+    state = np.arange(2**state_vector.num_qubits)
+    expected_norm = np.linalg.norm(state)
+    assert np.isclose(state_vector.norm(), expected_norm)
+
+
+def test_expectation_value(plus_state: StateVector) -> None:
+    operator = np.asarray([[1, 0], [0, -1]])  # Z gate
+    exp_value = plus_state.expectation(operator, [0])
+    expected_value = 0.0  # <++|Z|++> = 0
+    assert np.isclose(exp_value, expected_value)
