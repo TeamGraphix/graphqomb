ed8b0976670b38f59deb443f7258ef48604b852c
masa10-f <masa1063fuk@gmail.com>
2025-06-26 10:54:11 +0900

    add is_hermitian

--- graphix_zx/matrix.py
+++ graphix_zx/matrix.py
@@ -3,6 +3,7 @@
 This module provides:
 
 - `is_unitary`: check if a matrix is unitary.
+- `is_hermitian`: check if a matrix is Hermitian.
 """
 
 from __future__ import annotations
@@ -33,3 +34,19 @@ def is_unitary(mat: NDArray[T]) -> bool:
     if mat.shape[0] != mat.shape[1]:
         return False
     return np.allclose(np.eye(mat.shape[0]), mat @ mat.T.conj())
+
+
+def is_hermitian(mat: NDArray[T]) -> bool:
+    r"""Check if a matrix is Hermitian.
+
+    Parameters
+    ----------
+    mat : `numpy.typing.NDArray`\[T\]
+        matrix to check
+
+    Returns
+    -------
+    `bool`
+        `True` if Hermitian, `False` otherwise
+    """
+    return np.allclose(mat, mat.T.conj())
