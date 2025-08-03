97115bc47a48932fe0d56688b6a3d2dd5bba1948
masa10-f <masa1063fuk@gmail.com>
2025-08-03 14:39:09 +0900

    update scheduler class

--- graphix_zx/scheduler.py
+++ graphix_zx/scheduler.py
@@ -17,62 +17,20 @@ class Scheduler:
     ----------
     graph : `BaseGraphState`
         The graph state to be scheduled.
-    time_schedule : `dict`\[`int`, `tuple`\[`int`, `int`\]\]
-        A mapping from node indices to tuples representing the start and end times of each node's operations.
-        In this context, the node is prepared at the start time and measured at the end time.
+    prepare_time : `dict`\[`int`, `int | None`\]
+        A mapping from node indices to their preparation time.
+    measure_time : `dict`\[`int`, `int | None`\]
+        A mapping from node indices to their measurement time.
     """
 
     graph: BaseGraphState
-    __time_schedule: dict[int, tuple[int, int]]
+    prepare_time: dict[int, int | None]
+    measure_time: dict[int, int | None]
 
     def __init__(self, graph: BaseGraphState) -> None:
         self.graph = graph
-        self.__time_schedule = dict.fromkeys(graph.physical_nodes, (0, 1))
-
-    def num_slices(self) -> int:
-        """Get the number of slices in the schedule.
-
-        Returns
-        -------
-        `int`
-            The maximum end time across all nodes in the time schedule.
-        """
-        return max(end for _, end in self.__time_schedule.values())
-
-    @property
-    def time_schedule(self) -> dict[int, tuple[int, int]]:
-        """Get the time schedule."""
-        return self.__time_schedule
-
-    def prepare_time(self) -> dict[int, set[int]]:
-        r"""Get the preparation time for each node.
-
-        Returns
-        -------
-        `dict`\[`int`, `set`\[`int`\]
-            A mapping from node indices to sets of nodes that are prepared at the same time.
-        """
-        prepare_time: dict[int, set[int]] = {}
-        for node, (start, _) in self.__time_schedule.items():
-            if start not in prepare_time:
-                prepare_time[start] = set()
-            prepare_time[start].add(node)
-        return prepare_time
-
-    def measure_time(self) -> dict[int, set[int]]:
-        r"""Get the measurement time for each node.
-
-        Returns
-        -------
-        `dict`\[`int`, `set`\[`int`\]
-            A mapping from node indices to sets of nodes that are measured at the same time.
-        """
-        measure_time: dict[int, set[int]] = {}
-        for node, (_, end) in self.__time_schedule.items():
-            if end not in measure_time:
-                measure_time[end] = set()
-            measure_time[end].add(node)
-        return measure_time
+        self.prepare_time = dict.fromkeys(graph.physical_nodes - set(graph.input_node_indices))
+        self.measure_time = dict.fromkeys(graph.physical_nodes - set(graph.output_node_indices))
 
     def on_the_fly_from_grouping(self, grouping: Sequence[set[int]]) -> None:
         r"""Schedule graph preparation and measurements based on a grouping.
@@ -84,7 +42,7 @@ class Scheduler:
         """
         for i, group in enumerate(grouping):
             for node in group:
-                if i != len(grouping) - 1:
-                    self.__time_schedule[node] = (i, i + 2)
-                else:
-                    self.__time_schedule[node] = (i, i + 1)
+                if node not in self.graph.input_node_indices:
+                    self.prepare_time[node] = i
+                if node not in self.graph.output_node_indices:
+                    self.measure_time[node] = i + 2
