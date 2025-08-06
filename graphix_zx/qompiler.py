7a51968571ba89ce0f3cec6c5d7cc9eefc61ba32
masa10-f <masa1063fuk@gmail.com>
2025-08-06 21:26:04 +0900

    rename

--- graphix_zx/qompiler.py
+++ graphix_zx/qompiler.py
@@ -124,10 +124,10 @@ def _qompile(
             prepare_nodes, measure_nodes = schedule[time]
             commands.extend(N(node) for node in prepare_nodes)
             for node in measure_nodes:
-                for edge in graph.neighbors(node):
-                    if (node, edge) not in prepared_edges and (edge, node) not in prepared_edges:
-                        commands.append(E(nodes=(node, edge)))
-                        prepared_edges.add((node, edge))
+                for neighbor in graph.neighbors(node):
+                    if (node, neighbor) not in prepared_edges and (neighbor, node) not in prepared_edges:
+                        commands.append(E(nodes=(node, neighbor)))
+                        prepared_edges.add((node, neighbor))
             commands.extend(M(node, meas_bases[node]) for node in measure_nodes)
     if correct_output:
         commands.extend(X(node=node) for node in graph.output_node_indices)
