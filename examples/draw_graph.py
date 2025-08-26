207eddd96c5d13de66b92864b8d818aa39b3029a
Masato Fukushima <masa1063fuk@gmail.com>
2025-08-26 14:39:47 +0900

    set up for sphinx doc

--- examples/draw_graph.py
+++ examples/draw_graph.py
@@ -1,14 +1,16 @@
-"""Simple example to draw a GraphState in graphix-zx."""
+"""
+Graph Visualizer
+================
+
+Simple example to draw a GraphState in graphix-zx.
+"""
 
 # %%
-from graphix_zx.feedforward import dag_from_flow
 from graphix_zx.random_objects import generate_random_flow_graph
 from graphix_zx.visualizer import visualize
 
 # Create a random flow graph
 random_graph, flow = generate_random_flow_graph(5, 5)
-
-dag = dag_from_flow(random_graph, flow)
 
 
 # Visualize the flow graph
