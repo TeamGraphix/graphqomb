1239d314063232e354f3b41e498774930bf93420
masa10-f <masa1063fuk@gmail.com>
2025-09-08 19:04:58 +0900

    Revert "graphix_zx/visualizer.py"
    This reverts commit a23af3a6550338c3b83498e8740e86af141d591b.

--- graphix_zx/visualizer.py
+++ graphix_zx/visualizer.py
@@ -30,23 +30,24 @@ if TYPE_CHECKING:
 if sys.version_info >= (3, 11):
     from enum import StrEnum
 
-    class _ColorMap(StrEnum):
-        pass
+    class ColorMap(StrEnum):
+        """Color map for the nodes."""
+
+        XY = "#2ECC71"  # Emerald green
+        YZ = "#E74C3C"  # Vibrant red
+        XZ = "#3498DB"  # Bright blue
+        OUTPUT = "#95A5A6"  # Cool grey
 
 else:
     from enum import Enum
 
-    class _ColorMap(str, Enum):
-        pass
+    class ColorMap(str, Enum):
+        """Color map for the nodes."""
 
-
-class ColorMap(_ColorMap):
-    """Color map for the nodes."""
-
-    XY = "#2ECC71"  # Emerald green
-    YZ = "#E74C3C"  # Vibrant red
-    XZ = "#3498DB"  # Bright blue
-    OUTPUT = "#95A5A6"  # Cool grey
+        XY = "#2ECC71"  # Emerald green
+        YZ = "#E74C3C"  # Vibrant red
+        XZ = "#3498DB"  # Bright blue
+        OUTPUT = "#95A5A6"  # Cool grey
 
 
 def visualize(  # noqa: PLR0913
