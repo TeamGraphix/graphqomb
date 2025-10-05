d2172a979ccdac57e68a5fce41b16ee14164cb07
nabe98 <watanabeyuki@watanabeyuumarenoMacBook-Pro.local>
2025-10-05 17:53:34 +0900

    :sparkles: Implement local_complement

--- graphix_zx/zxgraphstate.py
+++ graphix_zx/zxgraphstate.py
@@ -7,7 +7,19 @@ This module provides:
 
 from __future__ import annotations
 
-from graphix_zx.graphstate import GraphState
+import operator
+from collections import defaultdict
+from itertools import combinations
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+from graphix_zx.common import Plane, PlannerMeasBasis, is_clifford_angle, is_close_angle
+from graphix_zx.euler import LocalClifford
+from graphix_zx.graphstate import BaseGraphState, GraphState, bipartite_edges
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Iterable
 
 
 class ZXGraphState(GraphState):
@@ -33,3 +45,70 @@ class ZXGraphState(GraphState):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def _update_connections(self, rmv_edges: Iterable[tuple[int, int]], new_edges: Iterable[tuple[int, int]]) -> None:
+        r"""Update the physical edges of the graph state.
+
+        Parameters
+        ----------
+        rmv_edges : `Iterable`\[`tuple`\[`int`, `int`\]\]
+            edges to remove
+        new_edges : `Iterable`\[`tuple`\[`int`, `int`\]\]
+            edges to add
+        """
+        for edge in rmv_edges:
+            self.remove_physical_edge(edge[0], edge[1])
+        for edge in new_edges:
+            self.add_physical_edge(edge[0], edge[1])
+
+    def local_complement(self, node: int) -> None:
+        r"""Local complement operation on the graph state: G*u.
+
+        Non-input node u gets Rx(pi/2) and its neighbors get Rz(-pi/2).
+        The edges between the neighbors of u are complemented.
+
+        Parameters
+        ----------
+        node : `int`
+            node index. The node must not be an input node.
+
+        Raises
+        ------
+        ValueError
+            If the node does not exist, is an input node, or the graph is not a ZX-diagram.
+        """
+        self._ensure_node_exists(node)
+        if node in self.input_node_indices:
+            msg = "Cannot apply local complement to input node."
+            raise ValueError(msg)
+        self._check_meas_basis()
+
+        nbrs: set[int] = self.neighbors(node)
+        nbr_pairs = complete_graph_edges(nbrs)
+        new_edges = nbr_pairs - self.physical_edges
+        rmv_edges = self.physical_edges & nbr_pairs
+
+        self._update_connections(rmv_edges, new_edges)
+
+        # apply local clifford to node and its neighbors
+        lc = LocalClifford(0, np.pi / 2, 0)
+        self.apply_local_clifford(node, lc)
+        lc = LocalClifford(-np.pi / 2, 0, 0)
+        for v in nbrs:
+            self.apply_local_clifford(v, lc)
+
+
+def complete_graph_edges(nodes: Iterable[int]) -> set[tuple[int, int]]:
+    r"""Return a set of edges for the complete graph on the given nodes.
+
+    Parameters
+    ----------
+    nodes : `Iterable`\[`int`\]
+        nodes
+
+    Returns
+    -------
+    `set`\[`tuple`\[`int`, `int`\]\]
+        edges of the complete graph
+    """
+    return {tuple(sorted((u, v))) for u, v in combinations(nodes, 2)}
---
+++ tests/test_zxgraphstate.py
@@ -0,0 +1,326 @@
+"""Tests for ZXGraphState
+
+Measurement actions for the followings are used:
+    - Local complement (LC): MEAS_ACTION_LC_*
+    - Pivot (PV): MEAS_ACTION_PV_*
+    - Remove Cliffords (RC): MEAS_ACTION_RC
+
+Reference:
+    M. Backens et al., Quantum 5, 421 (2021).
+    https://doi.org/10.22331/q-2021-03-25-421
+"""
+
+from __future__ import annotations
+
+import itertools
+import operator
+from copy import deepcopy
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pytest
+
+from graphix_zx.common import Plane, PlannerMeasBasis, is_close_angle
+from graphix_zx.random_objects import generate_random_flow_graph
+from graphix_zx.zxgraphstate import ZXGraphState
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from typing import Callable
+
+    Measurements = list[tuple[int, PlannerMeasBasis]]
+
+MEAS_ACTION_LC_TARGET: dict[Plane, tuple[Plane, Callable[[float], float]]] = {
+    Plane.XY: (Plane.XZ, lambda angle: angle + np.pi / 2),
+    Plane.XZ: (Plane.XY, lambda angle: -angle + np.pi / 2),
+    Plane.YZ: (Plane.YZ, lambda angle: angle + np.pi / 2),
+}
+MEAS_ACTION_LC_NEIGHBORS: dict[Plane, tuple[Plane, Callable[[float], float]]] = {
+    Plane.XY: (Plane.XY, lambda angle: angle + np.pi / 2),
+    Plane.XZ: (Plane.YZ, lambda angle: angle),
+    Plane.YZ: (Plane.XZ, operator.neg),
+}
+MEAS_ACTION_PV_TARGET: dict[Plane, tuple[Plane, Callable[[float], float]]] = {
+    Plane.XY: (Plane.YZ, operator.neg),
+    Plane.XZ: (Plane.XZ, lambda angle: (np.pi / 2 - angle)),
+    Plane.YZ: (Plane.XY, operator.neg),
+}
+MEAS_ACTION_PV_NEIGHBORS: dict[Plane, tuple[Plane, Callable[[float], float]]] = {
+    Plane.XY: (Plane.XY, lambda angle: (angle + np.pi) % (2.0 * np.pi)),
+    Plane.XZ: (Plane.XZ, lambda angle: -angle % (2.0 * np.pi)),
+    Plane.YZ: (Plane.YZ, lambda angle: -angle % (2.0 * np.pi)),
+}
+ATOL = 1e-9
+MEAS_ACTION_RC: dict[Plane, tuple[Plane, Callable[[float, float], float]]] = {
+    Plane.XY: (
+        Plane.XY,
+        lambda a_pi, alpha: (alpha if is_close_angle(a_pi, 0, ATOL) else alpha + np.pi) % (2.0 * np.pi),
+    ),
+    Plane.XZ: (
+        Plane.XZ,
+        lambda a_pi, alpha: (alpha if is_close_angle(a_pi, 0, ATOL) else -alpha) % (2.0 * np.pi),
+    ),
+    Plane.YZ: (
+        Plane.YZ,
+        lambda a_pi, alpha: (alpha if is_close_angle(a_pi, 0, ATOL) else -alpha) % (2.0 * np.pi),
+    ),
+}
+
+
+def plane_combinations(n: int) -> list[tuple[Plane, ...]]:
+    """Generate all combinations of planes of length n.
+
+    Parameters
+    ----------
+    n : int
+        The length of the combinations. n > 1.
+
+    Returns
+    -------
+    list[tuple[Plane, ...]]
+        A list of tuples containing all combinations of planes of length n.
+    """
+    return list(itertools.product(Plane, repeat=n))
+
+
+@pytest.fixture
+def rng() -> np.random.Generator:
+    return np.random.default_rng()
+
+
+@pytest.fixture
+def zx_graph() -> ZXGraphState:
+    """Generate an empty ZXGraphState object.
+
+    Returns
+    -------
+        ZXGraphState: An empty ZXGraphState object.
+    """
+    return ZXGraphState()
+
+
+def _initialize_graph(
+    zx_graph: ZXGraphState,
+    nodes: range,
+    edges: set[tuple[int, int]],
+    inputs: Sequence[int] = (),
+    outputs: Sequence[int] = (),
+) -> None:
+    """Initialize a ZXGraphState object with the given nodes and edges.
+
+    Parameters
+    ----------
+    zx_graph : ZXGraphState
+        The ZXGraphState object to initialize.
+    nodes : range
+        nodes to add to the graph.
+    edges : list[tuple[int, int]]
+        edges to add to the graph.
+    inputs : Sequence[int], optional
+        input nodes, by default ().
+    outputs : Sequence[int], optional
+        output nodes, by default ().
+
+    Raises
+    ------
+    ValueError
+        If the number of output nodes is greater than the number of input nodes.
+    """
+    for _ in nodes:
+        zx_graph.add_physical_node()
+    for i, j in edges:
+        zx_graph.add_physical_edge(i, j)
+
+    node2q_index: dict[int, int] = {}
+    q_indices: list[int] = []
+
+    for node in inputs:
+        q_index = zx_graph.register_input(node)
+        node2q_index[node] = q_index
+        q_indices.append(q_index)
+
+    if len(outputs) > len(q_indices):
+        msg = "Cannot assign valid q_index to all output nodes."
+        raise ValueError(msg)
+
+    for idx, node in enumerate(outputs):
+        q_index = node2q_index.get(node, idx)
+        zx_graph.register_output(node, q_index)
+
+
+def _apply_measurements(zx_graph: ZXGraphState, measurements: Measurements) -> None:
+    for node_id, planner_meas_basis in measurements:
+        if node_id in zx_graph.output_node_indices:
+            continue
+        zx_graph.assign_meas_basis(node_id, planner_meas_basis)
+
+
+def _test(
+    zx_graph: ZXGraphState,
+    exp_nodes: set[int],
+    exp_edges: set[tuple[int, int]],
+    exp_measurements: Measurements,
+) -> None:
+    assert zx_graph.physical_nodes == exp_nodes
+    assert zx_graph.physical_edges == exp_edges
+    for node_id, planner_meas_basis in exp_measurements:
+        assert zx_graph.meas_bases[node_id].plane == planner_meas_basis.plane
+        assert is_close_angle(zx_graph.meas_bases[node_id].angle, planner_meas_basis.angle)
+
+
+def test_local_complement_fails_if_nonexistent_node(zx_graph: ZXGraphState) -> None:
+    """Test local complement raises an error if the node does not exist."""
+    with pytest.raises(ValueError, match="Node does not exist node=0"):
+        zx_graph.local_complement(0)
+
+
+def test_local_complement_fails_if_not_zx_graph(zx_graph: ZXGraphState) -> None:
+    """Test local complement raises an error if the graph is not a ZX-diagram."""
+    zx_graph.add_physical_node()
+    with pytest.raises(ValueError, match="Measurement basis not set for node 0"):
+        zx_graph.local_complement(0)
+
+
+def test_local_complement_fails_with_input_node(zx_graph: ZXGraphState) -> None:
+    """Test local complement fails with input node."""
+    zx_graph.add_physical_node()
+    zx_graph.register_input(0)
+    with pytest.raises(ValueError, match=r"Cannot apply local complement to input node."):
+        zx_graph.local_complement(0)
+
+
+@pytest.mark.parametrize("plane", list(Plane))
+def test_local_complement_with_no_edge(zx_graph: ZXGraphState, plane: Plane, rng: np.random.Generator) -> None:
+    """Test local complement with a graph with a single node and no edges."""
+    angle = rng.random() * 2 * np.pi
+    ref_plane, ref_angle_func = MEAS_ACTION_LC_TARGET[plane]
+    ref_angle = ref_angle_func(angle)
+    zx_graph.add_physical_node()
+    zx_graph.assign_meas_basis(0, PlannerMeasBasis(plane, angle))
+
+    zx_graph.local_complement(0)
+    assert zx_graph.physical_edges == set()
+    assert zx_graph.meas_bases[0].plane == ref_plane
+    assert is_close_angle(zx_graph.meas_bases[0].angle, ref_angle)
+
+
+@pytest.mark.parametrize("planes", plane_combinations(3))
+def test_local_complement_with_minimal_graph(
+    zx_graph: ZXGraphState, planes: tuple[Plane, Plane, Plane], rng: np.random.Generator
+) -> None:
+    """Test local complement with a minimal graph."""
+    for _ in range(3):
+        zx_graph.add_physical_node()
+    for i, j in [(0, 1), (1, 2)]:
+        zx_graph.add_physical_edge(i, j)
+    angles = [rng.random() * 2 * np.pi for _ in range(3)]
+    for i in range(3):
+        zx_graph.assign_meas_basis(i, PlannerMeasBasis(planes[i], angles[i]))
+    zx_graph.local_complement(1)
+    ref_plane0, ref_angle_func0 = MEAS_ACTION_LC_NEIGHBORS[planes[0]]
+    ref_plane1, ref_angle_func1 = MEAS_ACTION_LC_TARGET[planes[1]]
+    ref_plane2, ref_angle_func2 = MEAS_ACTION_LC_NEIGHBORS[planes[2]]
+    ref_angle0 = ref_angle_func0(angles[0])
+    ref_angle1 = ref_angle_func1(angles[1])
+    ref_angle2 = ref_angle_func2(angles[2])
+    exp_measurements = [
+        (0, PlannerMeasBasis(ref_plane0, ref_angle0)),
+        (1, PlannerMeasBasis(ref_plane1, ref_angle1)),
+        (2, PlannerMeasBasis(ref_plane2, ref_angle2)),
+    ]
+    _test(zx_graph, exp_nodes={0, 1, 2}, exp_edges={(0, 1), (0, 2), (1, 2)}, exp_measurements=exp_measurements)
+
+    zx_graph.local_complement(1)
+    ref_plane0, ref_angle_func0 = MEAS_ACTION_LC_NEIGHBORS[ref_plane0]
+    ref_plane1, ref_angle_func1 = MEAS_ACTION_LC_TARGET[ref_plane1]
+    ref_plane2, ref_angle_func2 = MEAS_ACTION_LC_NEIGHBORS[ref_plane2]
+    exp_measurements = [
+        (0, PlannerMeasBasis(ref_plane0, ref_angle_func0(ref_angle0))),
+        (1, PlannerMeasBasis(ref_plane1, ref_angle_func1(ref_angle1))),
+        (2, PlannerMeasBasis(ref_plane2, ref_angle_func2(ref_angle2))),
+    ]
+    _test(
+        zx_graph,
+        exp_nodes={0, 1, 2},
+        exp_edges={(0, 1), (1, 2)},
+        exp_measurements=exp_measurements,
+    )
+
+
+@pytest.mark.parametrize(("plane1", "plane3"), plane_combinations(2))
+def test_local_complement_on_output_node(
+    zx_graph: ZXGraphState, plane1: Plane, plane3: Plane, rng: np.random.Generator
+) -> None:
+    """Test local complement on an output node."""
+    _initialize_graph(zx_graph, nodes=range(5), edges={(0, 1), (1, 2), (2, 3), (3, 4)}, inputs=(0, 4), outputs=(2,))
+    angle1 = rng.random() * 2 * np.pi
+    angle3 = rng.random() * 2 * np.pi
+    measurements = [
+        (0, PlannerMeasBasis(Plane.XY, 0.0)),
+        (1, PlannerMeasBasis(plane1, angle1)),
+        (3, PlannerMeasBasis(plane3, angle3)),
+        (4, PlannerMeasBasis(Plane.XY, 0.0)),
+    ]
+    _apply_measurements(zx_graph, measurements)
+    zx_graph.local_complement(2)
+
+    ref_plane1, ref_angle_func1 = MEAS_ACTION_LC_NEIGHBORS[plane1]
+    ref_plane3, ref_angle_func3 = MEAS_ACTION_LC_NEIGHBORS[plane3]
+    exp_measurements = [
+        (1, PlannerMeasBasis(ref_plane1, ref_angle_func1(measurements[1][1].angle))),
+        (3, PlannerMeasBasis(ref_plane3, ref_angle_func3(measurements[2][1].angle))),
+    ]
+    _test(
+        zx_graph,
+        exp_nodes={0, 1, 2, 3, 4},
+        exp_edges={(0, 1), (1, 2), (1, 3), (2, 3), (3, 4)},
+        exp_measurements=exp_measurements,
+    )
+    assert zx_graph.meas_bases.get(2) is None
+
+
+@pytest.mark.parametrize(("plane0", "plane1"), plane_combinations(2))
+def test_local_complement_with_two_nodes_graph(
+    zx_graph: ZXGraphState, plane0: Plane, plane1: Plane, rng: np.random.Generator
+) -> None:
+    """Test local complement with a graph with two nodes."""
+    zx_graph.add_physical_node()
+    zx_graph.add_physical_node()
+    zx_graph.add_physical_edge(0, 1)
+    angle0 = rng.random() * 2 * np.pi
+    angle1 = rng.random() * 2 * np.pi
+    zx_graph.assign_meas_basis(0, PlannerMeasBasis(plane0, angle0))
+    zx_graph.assign_meas_basis(1, PlannerMeasBasis(plane1, angle1))
+    zx_graph.local_complement(0)
+
+    ref_plane0, ref_angle_func0 = MEAS_ACTION_LC_TARGET[plane0]
+    ref_plane1, ref_angle_func1 = MEAS_ACTION_LC_NEIGHBORS[plane1]
+    exp_measurements = [
+        (0, PlannerMeasBasis(ref_plane0, ref_angle_func0(angle0))),
+        (1, PlannerMeasBasis(ref_plane1, ref_angle_func1(angle1))),
+    ]
+    _test(zx_graph, exp_nodes={0, 1}, exp_edges={(0, 1)}, exp_measurements=exp_measurements)
+
+
+@pytest.mark.parametrize("planes", plane_combinations(3))
+def test_local_complement_4_times(
+    zx_graph: ZXGraphState, planes: tuple[Plane, Plane, Plane], rng: np.random.Generator
+) -> None:
+    """Test 4 times local complement returns to the original graph."""
+    for _ in range(3):
+        zx_graph.add_physical_node()
+    for i, j in [(0, 1), (1, 2)]:
+        zx_graph.add_physical_edge(i, j)
+    angles = [rng.random() * 2 * np.pi for _ in range(3)]
+    for i in range(3):
+        zx_graph.assign_meas_basis(i, PlannerMeasBasis(planes[i], angles[i]))
+
+    for _ in range(4):
+        zx_graph.local_complement(1)
+
+    exp_measurements = [(i, PlannerMeasBasis(planes[i], angles[i])) for i in range(3)]
+    _test(zx_graph, exp_nodes={0, 1, 2}, exp_edges={(0, 1), (1, 2)}, exp_measurements=exp_measurements)
+
+
+if __name__ == "__main__":
+    pytest.main()
