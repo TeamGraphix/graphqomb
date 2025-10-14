2235f4c0803b1148ea3bcfca0ea7ad23028019d5
Masato Fukushima <masa1063fuk@gmail.com>
2025-10-14 11:35:45 +0900

    use stringio for better performance

--- graphix_zx/stim_compiler.py
+++ graphix_zx/stim_compiler.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from io import StringIO
 from typing import TYPE_CHECKING
 
 from graphix_zx.command import E, M, N
@@ -43,24 +44,24 @@ def stim_compile(  # noqa: C901, PLR0912
     ValueError
         If an unsupported measurement basis is encountered.
     """
-    stim_str = ""
+    stim_io = StringIO()
     meas_order: list[int] = []
     pframe = pattern.pauli_frame
     for input_node in pattern.input_node_indices:
-        stim_str += f"RX {input_node}\n"
+        stim_io.write(f"RX {input_node}\n")
         if after_clifford_depolarization > 0.0:
-            stim_str += f"DEPOLARIZE1({after_clifford_depolarization}) {input_node}\n"
+            stim_io.write(f"DEPOLARIZE1({after_clifford_depolarization}) {input_node}\n")
     for cmd in pattern:
         if isinstance(cmd, N):
             # prepare node in |+> state
-            stim_str += f"RX {cmd.node}\n"
+            stim_io.write(f"RX {cmd.node}\n")
             if after_clifford_depolarization > 0.0:
-                stim_str += f"DEPOLARIZE1({after_clifford_depolarization}) {cmd.node}\n"
+                stim_io.write(f"DEPOLARIZE1({after_clifford_depolarization}) {cmd.node}\n")
         if isinstance(cmd, E):
             q1, q2 = cmd.nodes
-            stim_str += f"CZ {q1} {q2}\n"
+            stim_io.write(f"CZ {q1} {q2}\n")
             if after_clifford_depolarization > 0.0:
-                stim_str += f"DEPOLARIZE2({after_clifford_depolarization}) {q1} {q2}\n"
+                stim_io.write(f"DEPOLARIZE2({after_clifford_depolarization}) {q1} {q2}\n")
         if isinstance(cmd, M):
             axis = determine_pauli_axis(cmd.meas_basis)
             if axis is None:
@@ -69,40 +70,34 @@ def stim_compile(  # noqa: C901, PLR0912
 
             if axis == Axis.X:
                 if before_measure_flip_probability > 0.0:
-                    stim_str += f"Z_ERROR({before_measure_flip_probability}) {cmd.node}\n"
-                stim_str += f"MX {cmd.node}\n"
+                    stim_io.write(f"Z_ERROR({before_measure_flip_probability}) {cmd.node}\n")
+                stim_io.write(f"MX {cmd.node}\n")
                 meas_order.append(cmd.node)
             elif axis == Axis.Y:
                 if before_measure_flip_probability > 0.0:
-                    stim_str += f"X_ERROR({before_measure_flip_probability}) {cmd.node}\n"
-                    stim_str += f"Z_ERROR({before_measure_flip_probability}) {cmd.node}\n"
-                stim_str += f"MY {cmd.node}\n"
+                    stim_io.write(f"X_ERROR({before_measure_flip_probability}) {cmd.node}\n")
+                    stim_io.write(f"Z_ERROR({before_measure_flip_probability}) {cmd.node}\n")
+                stim_io.write(f"MY {cmd.node}\n")
                 meas_order.append(cmd.node)
             elif axis == Axis.Z:
                 if before_measure_flip_probability > 0.0:
-                    stim_str += f"X_ERROR({before_measure_flip_probability}) {cmd.node}\n"
-                stim_str += f"MZ {cmd.node}\n"
+                    stim_io.write(f"X_ERROR({before_measure_flip_probability}) {cmd.node}\n")
+                stim_io.write(f"MZ {cmd.node}\n")
                 meas_order.append(cmd.node)
 
     x_check_groups, z_check_groups = pframe.detector_groups()
     for x_checks in x_check_groups:
-        target_str = ""
-        for x_check in x_checks:
-            target_str += f"rec[{meas_order.index(x_check) - len(meas_order)}] "
-        stim_str += f"DETECTOR {target_str.strip()}\n"
+        targets = [f"rec[{meas_order.index(x_check) - len(meas_order)}]" for x_check in x_checks]
+        stim_io.write(f"DETECTOR {' '.join(targets)}\n")
     for z_checks in z_check_groups:
-        target_str = ""
-        for z_check in z_checks:
-            target_str += f"rec[{meas_order.index(z_check) - len(meas_order)}] "
-        stim_str += f"DETECTOR {target_str.strip()}\n"
+        targets = [f"rec[{meas_order.index(z_check) - len(meas_order)}]" for z_check in z_checks]
+        stim_io.write(f"DETECTOR {' '.join(targets)}\n")
 
     # logical observables
     if logical_observables is not None:
         for log_idx, obs in logical_observables.items():
             logical_observables_group = pframe.logical_observables_group(obs)
-            target_str = ""
-            for node in logical_observables_group:
-                target_str += f"rec[{meas_order.index(node) - len(meas_order)}] "
-            stim_str += f"OBSERVABLE_INCLUDE({log_idx}) {target_str.strip()}\n"
+            targets = [f"rec[{meas_order.index(node) - len(meas_order)}]" for node in logical_observables_group]
+            stim_io.write(f"OBSERVABLE_INCLUDE({log_idx}) {' '.join(targets)}\n")
 
-    return stim_str.strip()
+    return stim_io.getvalue().strip()
